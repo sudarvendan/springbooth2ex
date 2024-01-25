05013cfc2bd8738089e152dbbc5e80b45549b956
sudarvendan <sudarvendan@gmail.com>
2024-01-25 09:15:45 +0530

    Update Configuration Structure

--- src/main/java/com/javalearning/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.javalearning.repository;
-
-import com.javalearning.model.Student;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StudentRepository extends CrudRepository<Student, Integer> {
-
-}
--- src/main/java/com/javalearning/springbooth2ex/service/StudentService.java
+++ src/main/java/com/javalearning/springbooth2ex/service/StudentService.java
@@ -2,20 +2,18 @@ package com.javalearning.springbooth2ex.service;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 import com.javalearning.springbooth2ex.model.Student;
 import com.javalearning.springbooth2ex.repository.StudentRepository;
 
 @Service
-@Configuration
 public class StudentService {
-    StudentRepository studentRepository;
 
     @Autowired
-    @Bean
+    StudentRepository studentRepository;
+
     public List<Student> getAllStudent(){
         List<Student> students = new ArrayList<Student>();
         studentRepository.findAll().forEach(student -> students.add(student));
